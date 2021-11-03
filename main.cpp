4a4d846211435a5928b18fe5cf19623412304bc2
gdecker2002 <77694417+gdecker2002@users.noreply.github.com>
2021-11-03 13:42:02 -0500

    Created Student Class
    variables: first, last, grade, pin, cost of lunch
    
    Getters and setters included

--- main.cpp
+++ main.cpp
@@ -1,7 +1,54 @@
-#include <QCoreApplication>
+#include <iostream>
+using namespace std;
 
+class Student {
+  private:
+    string fname;
+    string lname;
+    string grade;
+    int pin; //should be a unique 4 digit number
+    double costOfLunch; //student cost of lunch (useful for reduced/free lunch)
+  public:
+    void setFirst(string input) {
+        fname = input;
+    }
+    string getFirst() {
+        return fname;
+    }
+    void setLast(string input) {
+        lname = input;
+    }
+    string getLast() {
+        return lname;
+    }
+    void setGrade(string input) {
+        grade = input;
+    }
+    string getGrade() {
+        return grade;
+    }
+    void setPin(int input) {
+        int size = to_string(input).length();
+        if(size == 4) {
+            pin = input;
+        }
+        else {
+            cout << "Pin should be 4 digits long." << endl;
+        }
+    }
+    int getPin() {
+        return pin;
+    }
+    void setLunch(double input) {
+        costOfLunch = input;
+    }
+    int getLunch() {
+        return costOfLunch;
+    }
+};
 int main()
 {
-
+    string userInputString = "";
+    int userInputInt;
     return 0;
 }
