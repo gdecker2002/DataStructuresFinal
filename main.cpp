94f2cf2e0e9b910f7e21be7877c2b3285173229b
gdecker2002 <77694417+gdecker2002@users.noreply.github.com>
2021-11-09 11:12:53 -0600

    Created GUI
    Created GUI using menus

--- main.cpp
+++ main.cpp
@@ -49,6 +49,44 @@ class Student {
 int main()
 {
     string userInputString = "";
-    int userInputInt;
+    int userInputInt = -1;
+    while(userInputString != "stop") {
+    cout << "Please Select an Option\n --------------------------------------\n";
+    cout << "1. Take Lunch Attendance\n2. Display Lunch Attendance \n3. Lunch Help - Sides \n4. Lunch Help - Drinks \n";
+    cin >> userInputString;
+    while(userInputString != "1" && userInputString != "2" && userInputString != "3" && userInputString != "4") {
+        cout << "Invalid Choice (1-4): ";
+        cin >> userInputString;
+    }
+    if(userInputString == "1") {
+        cout << "\nEnter '0' at any point to return to the main menu\n--------------------------------------\n";
+        while(userInputString != "0") {
+            cout << "Enter lunch number: "; //Need to loop through students Pin numbers and make sure its a real pin
+            cin >> userInputString;
+        }
+            //Function here for if PIN is real, loop through student array
+
+        }
+    else if(userInputString == "2") {
+        cout << "\nLunch Attendance: \n--------------------------------------\n";
+
+        //Loop through lunch attendance and display all needed info (fname, lname, pin, etc.)
+
+    }
+    else if(userInputString == "3") {
+        cout << "\nLunch Help - Sides\n --------------------------------------\n";
+
+        //Implement binary decision tree
+
+    }
+    else {
+        cout << "\nLunch Help - Drink\n --------------------------------------\n";
+
+        //Implement binary decision tree
+
+    }
+    cout << "\nenter 'stop' to end program\n\n";
+
+    }
     return 0;
 }
